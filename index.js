2f423b7ae82cfea743089664b08fd40993c7d9aa
Anri Kawahara <anri0806@hotmail.co.jp>
2021-04-26 20:20:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,5 @@ function currentLine(line) {
  if (line == 0) {
    return "The line is currently empty."
  }
-  return `The line is currently: ${line(0), ${line(1)}} `
-
+  return `The line is currently: ${line(0), ${line(1)}`
 }
